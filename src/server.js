5f2203cd09a38003f7ea6593e5362b89a44e4459
Ali <kultaiali670@gmail.com>
2025-12-13 11:43:33 +0500

    update cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 const allowedOrigins = [
   "https://medical-project-orpin.vercel.app",
   "http://localhost:5173",
-  "https://food-recipes-eight-ivory.vercel.app/",
+  "https://food-recipes-eight-ivory.vercel.app",
 ];
 
 app.use(
